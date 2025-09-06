885a560ca1ee2a84e0f59b44829c948684d909d0
Nice Zombies <nineteendo19d0@gmail.com>
2025-09-06 14:21:13 +0200

    Use braille characters

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -3,35 +3,34 @@ from __future__ import annotations
 
 __all__: list[str] = ["convert_to_ascii"]
 
+from typing import TYPE_CHECKING
 import sys
 from math import ceil, floor
 
 import numpy as np
-from numpy.typing import NDArray
 from PIL import Image
 
+if TYPE_CHECKING:
+    _Box = tuple[int, int, int, int]
+
 _SCALE: float = 0.43
-_CHARS: str = '`"+1nuL0qpdbo8$'
+_CHARS: str = '⠀⡀⣀⣄⣤⣦⣶⣷⣿'
 
 
-def _format_ms(ms: float) -> str:
-    hh, remainder = divmod(int(ms), 3_600_000)
+def _format_ms(ms: int) -> str:
+    hh, remainder = divmod(ms, 3_600_000)
     mm, remainder = divmod(remainder, 60_000)
     ss, mss = divmod(remainder, 1_000)
     return f"{hh:02d}:{mm:02d}:{ss:02d},{mss:03d}"
 
 
-def _get_avg_brightness(
-    img: Image.Image, box: tuple[int, int, int, int]
-) -> float:
-    arr: NDArray = np.array(img.crop(box))
-    width, height = arr.shape
-    return np.average(arr.reshape(width * height))
+def _get_avg_brightness(img: Image.Image, box: _Box) -> float:
+    return np.average(np.array(img.crop(box)))
 
 
-def _get_char(img: Image.Image, box: tuple[int, int, int, int]) -> str:
+def _get_char(img: Image.Image, box: _Box) -> str:
     avg_brightness: float = _get_avg_brightness(img, box)
-    char: str = _CHARS[int(avg_brightness / 255 * (len(_CHARS) - 1))]
+    char: str = _CHARS[round(avg_brightness / 255 * (len(_CHARS) - 1))]
     if char in {'"', '`'}:
         char += ' '
 
@@ -72,6 +71,6 @@ def convert_to_ascii(
     """Convert a video frame to an SRT subtitle entry with ASCII art."""
     ascii_img: str = _convert_img_to_ascii(frame, rows)
     ms: float = frame_num * ms_per_frame + submsoffset
-    start_time: str = _format_ms(ms)
-    end_time: str = _format_ms(ms + ms_per_frame)
+    start_time: str = _format_ms(floor(ms))
+    end_time: str = _format_ms(ceil(ms + ms_per_frame))
     return f'{frame_num + 1}\n{start_time} --> {end_time}\n{ascii_img}'
