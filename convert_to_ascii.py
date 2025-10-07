576e77214bdfd5a513a2a44774fbab902429da5e
Nice Zombies <nineteendo19d0@gmail.com>
2025-10-07 15:13:09 +0200

    Use lanczos image resampling

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -4,8 +4,7 @@ from __future__ import annotations
 __all__: list[str] = ["convert_to_ascii"]
 
 from typing import TYPE_CHECKING, Any
-import sys
-from math import ceil, floor, inf
+from math import inf
 
 import numpy as np
 from numpy.typing import NDArray
@@ -14,44 +13,7 @@ from PIL import Image
 from convert_to_frames import CHAR_ASPECT_RATIO
 
 if TYPE_CHECKING:
-    _Box = tuple[float, float, float, float]
     _Color = NDArray
-
-
-def _get_avg_color(arr: NDArray, box: _Box) -> _Color:
-    x1, y1, x2, y2 = box
-    x1, y1, x2, y2 = floor(x1), floor(y1), ceil(x2), ceil(y2)
-    total_color: _Color = arr[y2 - 1, x2 - 1].copy()
-    if x1 > 0 and y1 > 0:
-        total_color += arr[y1 - 1, x1 - 1]
-
-    if y1 > 0:
-        total_color -= arr[y1 - 1, x2 - 1]
-
-    if x1 > 0:
-        total_color -= arr[y2 - 1, x1 - 1]
-
-    return total_color / ((y2 - y1) * (x2 - x1))
-
-
-# pylint: disable-next=R0914
-def _get_avg_colors(arr: NDArray, box: _Box) -> NDArray:
-    x1, y1, x2, y2 = box
-    sub_pixel_width: float = (x2 - x1) / 2
-    sub_pixel_height: float = (y2 - y1) / 4
-    avg_colors: NDArray = np.empty((8, 3))
-    idx: int = 0
-    for j in range(4):
-        sub_y1: float = y1 + j * sub_pixel_height
-        sub_y2: float = sub_y1 + sub_pixel_height
-        for i in range(2):
-            sub_x1: float = x1 + i * sub_pixel_width
-            sub_x2: float = sub_x1 + sub_pixel_width
-            sub_box: _Box = sub_x1, sub_y1, sub_x2, sub_y2
-            avg_colors[idx] = _get_avg_color(arr, sub_box)
-            idx += 1
-
-    return avg_colors
 
 
 def _get_dev(x: NDArray) -> float:
@@ -86,8 +48,8 @@ def _color2id(color: _Color) -> int:
     return 256 * r + 16 * g + b
 
 
-def _get_colored_char(arr: NDArray, box: _Box) -> tuple[int, str]:
-    colors: NDArray = _get_avg_colors(arr, box)
+def _get_colored_char(sub_arr: NDArray) -> tuple[int, str]:
+    colors: NDArray = sub_arr.reshape(-1, 3)
     idxs: NDArray = _get_best_idxs(colors)
     color_id: int = _color2id(colors[idxs].mean(0)) if len(idxs) else 0
     value: int = 0x2800
@@ -101,14 +63,9 @@ def _get_colored_char(arr: NDArray, box: _Box) -> tuple[int, str]:
 def _convert_img_to_ascii(
     palette: dict[int, int], img: Image.Image, rows: int
 ) -> tuple[int, str]:
-    arr: NDArray = np.array(img).cumsum(axis=0).cumsum(axis=1)
     cols: int = round(rows / CHAR_ASPECT_RATIO * img.width / img.height)
-    pixel_height: float = img.height / rows
-    pixel_width: float = img.width / cols
-    if cols > img.width or rows > img.height:
-        print("Image too small for specified rows!")
-        sys.exit(1)
-
+    img = img.resize((2 * cols, 4 * rows), Image.Resampling.LANCZOS)
+    arr: NDArray = np.array(img)
     ascii_img: list[str] = []
     prev_color_id: int = -1
     first_palette_id: int = -1
@@ -116,12 +73,9 @@ def _convert_img_to_ascii(
         if j:
             ascii_img.append('\n')
 
-        y1: float = j * pixel_height
-        y2: float = (j + 1) * pixel_height
         for i in range(cols):
-            x1: float = i * pixel_width
-            x2: float = (i + 1) * pixel_width
-            color_id, char = _get_colored_char(arr, (x1, y1, x2, y2))
+            sub_arr: NDArray = arr[4 * j:4 * j + 4, 2 * i:2 * i + 2]
+            color_id, char = _get_colored_char(sub_arr)
             if color_id != prev_color_id:
                 palette_id: int = palette.setdefault(color_id, len(palette))
                 if first_palette_id == -1:
